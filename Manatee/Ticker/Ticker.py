8bcac9bf08831170a83d1b4a8a71095a7ff24af3
Ohad Ozer <ohad.ozer@cellebrite.com>
2019-09-12 23:28:18 +0300

    ticker: some more basic impl

--- Manatee/Ticker/Ticker.py
+++ Manatee/Ticker/Ticker.py
@@ -1,23 +1,29 @@
 from os.path import dirname, abspath, join
 from alpha_vantage.timeseries import TimeSeries
-
+from enum import Enum
 
 class TickerError(Exception):
-    
+
     def __init__(self, desc):
         desc = 'Manatee Ticker Error :: ' + desc
         super(TickerError, self).__init__(desc)
 
+class TickerResultTimeFrame(Enum):
+    Daily = 1
+    Weekly = 1
+    Monthly = 3
+    Yearly = 4
 
 class TickerResult:
 
     def __init__(self):
-        pass
+        self._tf = TickerResultTimeFrame.Daily
+        self._data = {}
 
 
 class Ticker:
 
-    def __init__(self, api_key=None):
+    def __init__(self, api_key=None, timeframe=TickerResultTimeFrame.Daily, tolerance=0.1):
 
         if api_key:
             self._api_key = api_key
@@ -28,9 +34,17 @@ class Ticker:
             except Exception as ex:
                 raise TickerError('Error reading API key. %s' % str(ex))
 
-        self._ts = None
+        if 0.001 < tolerance < 30:
+            self._tolerance = tolerance
+        else:
+            raise TickerError('Invalid tolerance')
 
-        self._create_internal_endpoint()
+        if isinstance(timeframe, TickerResultTimeFrame):
+            self._tf = timeframe
+        else:
+            raise TickerError('Invalid timeframe')
+
+        self._ts = self._create_internal_endpoint()
 
     def _create_internal_endpoint(self):
 
@@ -38,7 +52,7 @@ class Ticker:
             raise TickerError('Invalid API key')
 
         try:
-            self._ts = TimeSeries(key=self._api_key)
+            return TimeSeries(key=self._api_key)
         except ValueError as ve:
             raise TickerError('Internal error on TS creation. %s' % str(ve))
 
@@ -46,6 +60,21 @@ class Ticker:
 
         res = TickerResult()
 
-        data, metadata = self._ts.get_daily('SPX', outputsize='full')
+        data, metadata = None
+
+        try:
+            if self._tf == TickerResultTimeFrame.Daily:
+                data, metadata = self._ts.get_daily('SPX', outputsize='full')
+            if self._tf == TickerResultTimeFrame.Weekly:
+                data, metadata = self._ts.get_weekly('SPX', outputsize='full')
+            if self._tf == TickerResultTimeFrame.Monthly:
+                data, metadata = self._ts.get_monthly('SPX', outputsize='full')
+        except Exception as ex:
+            raise TickerError('Error getting time series data. %s' % str(ex))
+
+        if not data:
+            raise TickerError('Empty time series data result')
+
+        res._tf = self._tf
 
         return res
